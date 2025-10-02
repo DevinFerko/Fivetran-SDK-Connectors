6e15624ed873fd0bd3e5344e476248142107f5d2
Devin Ferko <devin.ferko@outlook.com>
2025-10-02 10:06:01 +0100

    Start of script

---
+++ Livechat/connector.py
@@ -0,0 +1,19 @@
+"""This connector demonstrates how to fetch data from Livechat API and upsert it into destination using the Fivetran Connector SDK.
+See the Technical Reference documentation (https://fivetran.com/docs/connectors/connector-sdk/technical-reference#update)
+and the Best Practices documentation (https://fivetran.com/docs/connectors/connector-sdk/best-practices) for details
+"""
+
+# Import required classes from fivetran_connector_sdk
+# For supporting Connector operations like Update() and Schema()
+from fivetran_connector_sdk import Connector
+
+# For enabling Logs in your connector code
+from fivetran_connector_sdk import Logging as log
+
+# For supporting Data operations like Upsert(), Update(), Delete() and checkpoint()
+from fivetran_connector_sdk import Operations as op
+
+# Import required libraries
+import json  # For JSON data handling and serialization
+import requests  # For making HTTP requests to the Checkly API
+import time  # For handling time-related functions like sleep for rate limiting
