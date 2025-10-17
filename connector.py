b0d7ed4c537593e1cb01e283fc781276ff6cc9e6
Devin Ferko <devin.ferko@outlook.com>
2025-10-17 10:36:51 +0100

    Intialize connector and command line function

--- connector.py
+++ connector.py
@@ -4,6 +4,7 @@ This connector module provides the necessary classes and functions to integrate 
 
 # Import necessary modules
 import json
+import requests
 from fivetran_connector_sdk import Connector, Operations as op, Logging as log
 
 # Message to check key connectivity
@@ -13,10 +14,35 @@ CONNECTIVITY_MESSAGE = "Successfully connected to LiveChat API. Key is valid."
 def schema(configuration: dict):
     if 'my_key' not in configuration:
         raise ValueError("Configuration must include 'my_key'.")
+    
+
+
     return [
         {
-            "table": "chats",
-            "primary_key": ["chat_id"]
+            "table": "lists_chats", 
+            "primary_key": ["id"]
         }
     ]
 
+def update(configuration: dict, state: dict):
+    # Placeholder for update logic
+    return state
+
+
+
+
+
+# Create the connector object using the schema and update functions
+connector = Connector(update=update, schema=schema)
+
+# Check if the script is being run as the main module.
+# This is Python's standard entry method allowing your script to be run directly from the command line or IDE 'run' button.
+# This is useful for debugging while you write your code. Note this method is not called by Fivetran when executing your connector in production.
+# Please test using the Fivetran debug command prior to finalizing and deploying your connector.
+if __name__ == "__main__":
+    # Open the configuration.json file and load its contents
+    with open("configuration.json", "r") as f:
+        configuration = json.load(f)
+
+    # Test the connector locally
+    connector.debug(configuration=configuration)
