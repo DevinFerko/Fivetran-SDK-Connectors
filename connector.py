0bbc4e2a4530416474dbd6af7a69fd6dd57580b0
Devin Ferko <devin.ferko@outlook.com>
2025-10-17 08:47:25 +0100

    Define Schema

--- connector.py
+++ connector.py
@@ -8,3 +8,15 @@ from fivetran_connector_sdk import Connector, Operations as op, Logging as log
 
 # Message to check key connectivity
 CONNECTIVITY_MESSAGE = "Successfully connected to LiveChat API. Key is valid."
+
+# Defines Schema for the connector
+def schema(configuration: dict):
+    if 'my_key' not in configuration:
+        raise ValueError("Configuration must include 'my_key'.")
+    return [
+        {
+            "table": "chats",
+            "primary_key": ["chat_id"]
+        }
+    ]
+
